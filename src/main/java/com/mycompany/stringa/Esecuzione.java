bf1361f2245869f066bb2703afa1359f1882ca83
asocc <asocc@LAPTOP-17THED63.lan>
2024-11-16 23:09:48 +0100

    parole

---
+++ src/main/java/com/mycompany/stringa/Esecuzione.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.stringa;
+
+
+
+/**
+ *
+ * @author asocc
+ */
+public class Esecuzione {
+      public static void main(String[] args) {
+        Stringa.Lettore();
+    }
+}
+
+/*
+    private static class Lettore {
+    Stringa Lettore= new Stringa(); scanner.scrittura();
+        public Lettore() {
+        }*/ 
+  
---
+++ src/main/java/com/mycompany/stringa/Stringa.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.stringa;
+
+/**
+ *
+ * @author asocc
+ */
+import java.util.Scanner;
+
+public class Stringa {
+    private String scrittura;
+    public String getScrittura() {
+        return scrittura;
+    }
+ 
+
+   public static void Lettore() {
+        Scanner lettore = new Scanner(System.in);
+        System.out.print("Scrivi una parola: ");
+        String parola = lettore.nextLine();
+        System.out.println("Hai scritto: " + parola);
+    }
+   }
+   
