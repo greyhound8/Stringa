b3429844b8884c44035503643c390cdc1596ed7d
greyhound8 <asoccio045@gmail.com>
2024-11-18 22:05:16 +0100

    Update Stringa.java

--- src/main/java/com/mycompany/stringa/Stringa.java
+++ src/main/java/com/mycompany/stringa/Stringa.java
@@ -11,17 +11,23 @@ package com.mycompany.stringa;
 import java.util.Scanner;
 
 public class Stringa {
+    //crea una variabile privata chiamata scrittura 
     private String scrittura;
+    // Metodo getter per la variabile scrittura
     public String getScrittura() {
+      //fa ritornare la variabile scrittura allo stato iniziale
         return scrittura;
     }
  
-
+// Metodo che legge una parola dall'input dell'utente
    public void Lettore() {
+       // Metodo che legge una parola dall'input dell'utente
         Scanner lettore = new Scanner(System.in);
+        // Chiede all'utente di scrivere una parola
         System.out.print("Scrivi una parola: ");
-        String parola = lettore.nextLine();
-        System.out.println("Hai scritto: " + parola);
+        //visualizza la risposta dell'utente
+        System.out.println("Hai scritto: ");
+        //la memorizza nella variabile scrittura
+        scrittura = lettore.nextLine();
     }
    }
-   
